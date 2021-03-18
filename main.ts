6fb74a4193e77fdfdb5ecbfc18d8217557616be5
María Mateo Vázquez -UCJC- <memematvaz@gmail.com>
2021-03-18 12:29:30 +0100

    initial-commit

---
+++ main.ts
@@ -0,0 +1,42 @@
+class Person{
+    private _name: string;
+    private _surname: string;
+    private _age: number;
+    private _dni: string;
+    private _birthday: string;
+    private _color: string;
+    private _sex: string;
+    private _address: string;
+    private _mail: string;
+    private _phone: number;
+    private _notes: string;
+
+   
+    constructor(
+        name: string, 
+        surname: string, 
+        age: number, 
+        dni: string, 
+        birthday: string, 
+        color: string, 
+        sex: string, 
+        address: string,
+        mail: string,
+        phone: number,
+        notes: string) {
+       //sospecho que esto es para predefinir el valor???? idk
+            this._name = name;
+            this._surname= surname;
+            this._age = age;
+            this._dni= dni;
+            this._birthday = birthday;
+            this._color= color;
+            this._sex = sex;
+            this._address= address;
+            this._mail = mail;
+            this._phone= phone;
+            this._notes = notes;
+
+        }
+        
+    }
